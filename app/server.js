ba5ebd611348b5979dec11b59d2f1c1900527ec1
Disa A. Syuhada <disaada@gmail.com>
2020-08-19 17:11:52 +0700

    add CRUD with sequelize

--- app/server.js
+++ app/server.js
@@ -1,19 +1,29 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 4000
 
 //set template engine twig
 app.set('view engine', 'twig')
+//untuk JSON
+app.use(bodyParser.urlencoded({extended: true}))
+
+//impor data objek dari file routes/user.js
+const message = require('../routes/user')
+app.get('/message/', message.newMessage)
+app.get('/message/show', message.showMessage)
+app.post('/message/new', message.postMessage)
+app.post('/message/:id/update', message.updateMessage)
+app.get('/message/:msg', message.getMessage)
+app.get('/message_id/:msg', message.getId)
+app.get('/message/:id/delete', message.deleteMessage)
+app.get('/message/:id/edit', message.editMessage)
 
 //menampilkan 'welcome' di browser
 app.get('/', (req, res) => {
     res.send('welcome')
 })
-
-//impor data objek dari file routes/user.js
-const message = require('../routes/user')
-app.get('/user/:msg', message.varMessage)
 
 app.listen(port, '127.0.0.1', () => {
 	console.log(`Example app listening as http://localhost:${port}`)
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,112 @@
+const { Sequelize, DataTypes, Op } = require('sequelize')
+const sequelize = new Sequelize({
+    dialect: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'employee_data',
+    password: '123',
+    database: 'employee_data'
+})
+
+const User = sequelize.define('message',
+    {
+        id: {
+            type: DataTypes.NUMBER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        message: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+    }, {
+        tableName: 'message',
+        timestamps: false
+    }
+)
+
 const getMessage = (req, res) => {
-    res.render('index', {varMessage: req.params.msg})
+    const data = {message : req.params.msg}
+    res.render('message', data)
+}
+
+const newMessage = (req, res) => {
+    res.render('form_create')
+}
+
+const postMessage = (req, res) => {
+    //res.send(req.body)
+    User.create(req.body).then(
+        (msg) => res.redirect('/message/show'),
+        (err) => res.send(err)
+    )
+}
+
+const showMessage = (req, res) => {
+    User.findAll().then(
+        (data) => res.render('show_message', { data }),
+        (err) => res.send(err)
+    )
+}
+
+const deleteMessage = (req, res) => {
+    User.destroy({ where: {
+        id : {
+            [Op.eq]: req.params.id
+        }
+    }}).then(
+        () => res.redirect('/message/show'),
+        (err) => res.send(err)
+    )
+}
+
+const getId = (req, res) => {
+    User.findOne({
+        where: {
+            id: {
+                [Op.eq]: req.params.msg
+            }
+        }
+    }).then(
+        (data) => {
+            res.send(data)
+            //res.render('message', data)
+        },
+        (err) => res.send(err)
+    )
+}
+
+const editMessage = (req, res) => {
+    User.findByPk(req.params.id).then(
+        (data) => {
+           res.render('edit_message', { data })
+        },
+        (err) => res.send(err)
+    )
+}
+
+const updateMessage = (req, res) => {
+    User.update(
+        req.body,
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    ).then(
+        (data) => res.redirect('/message/show'),
+        (err) => res.send(err)
+    )
 }
 
 //ekspor hasil pengambilan objek
-module.exports = {getMessage}
+module.exports = {
+    getMessage,
+    getId,
+    newMessage,
+    postMessage,
+    showMessage,
+    deleteMessage,
+    editMessage,
+    updateMessage
+}
