27e80ad2e9114784066a7f52952d55092ee5bb12
Disa A. Syuhada <disaada@gmail.com>
2020-08-20 17:16:27 +0700

    adding .editorconfig, tidy up the code and modify showMessage to sending data for React

--- models/message.js
+++ models/message.js
@@ -1,93 +1,107 @@
-const { Op } = require('sequelize')
+const {
+  Op
+} = require('sequelize')
 const User = require('../models/user')
 
 //show messages
 const showMessage = (req, res) => {
-    User.findAll().then(
-        (data) => {
-                if(req.xhr){
-                    res.send(data)    
-                } else {
-                    res.render('show_message', { data })
-                }
-            },
-        (err) => res.send(err)
-    )
+  User.findAll().then(
+    (data) => {
+      /* if(req.xhr){
+          res.send(data)
+      } else {
+          res.render('show_message', { data })
+      } */
+      if (req.headers.accept === 'application/json') {
+        res.send(data)
+      } else {
+        res.render('show_message', {
+          data
+        })
+      }
+    },
+    (err) => res.send(err)
+  )
 }
 //end show messages
 
 //show messages by id
 const getId = (req, res) => {
-    User.findOne({
-        where: {
-            id: {
-                [Op.eq]: req.params.msg
-            }
-        }
-    }).then(
-        (data) => {
-            res.send(data)
-            //res.render('message', data)
-        },
-        (err) => res.send(err)
-    )
+  User.findOne({
+    where: {
+      id: {
+        [Op.eq]: req.params.msg
+      }
+    }
+  }).then(
+    (data) => {
+      res.send(data)
+      //res.render('message', data)
+    },
+    (err) => res.send(err)
+  )
 }
 //end show messages by id
 
 //add messages
 const newMessage = (req, res) => {
-    res.render('form_create')
+  res.render('form_create')
 }
 
 const postMessage = (req, res) => {
-    //res.send(req.body)
-    User.create(req.body).then(
-        (msg) => res.redirect('/message'),
-        (err) => res.send(err)
-    )
+  //res.send(req.body)
+  User.create(req.body).then(
+    (msg) => res.redirect('/message'),
+    (err) => res.send(err)
+  )
 }
 //end add messages
 
 //edit messages
 const getMessage = (req, res) => {
-    const data = {message : req.params.msg}
-    res.render('message', data)
+  const data = {
+    message: req.params.msg
+  }
+  res.render('message', data)
 }
 
 const editMessage = (req, res) => {
-    User.findByPk(req.params.id).then(
-        (data) => {
-           res.render('edit_message', { data })
-        },
-        (err) => res.send(err)
-    )
+  User.findByPk(req.params.id).then(
+    (data) => {
+      res.render('edit_message', {
+        data
+      })
+    },
+    (err) => res.send(err)
+  )
 }
 
 const updateMessage = (req, res) => {
-    User.update(
-        req.body,
-        {
-            where: {
-                id: req.params.id
-            }
-        }
-    ).then(
-        (data) => res.redirect('/message'),
-        (err) => res.send(err)
-    )
+  User.update(
+    req.body, {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then(
+    (data) => res.redirect('/message'),
+    (err) => res.send(err)
+  )
 }
 //end edit messages
 
 //delete messages
 const deleteMessage = (req, res) => {
-    User.destroy({ where: {
-        id : {
-            [Op.eq]: req.params.id
-        }
-    }}).then(
-        () => res.redirect('/message'),
-        (err) => res.send(err)
-    )
+  User.destroy({
+    where: {
+      id: {
+        [Op.eq]: req.params.id
+      }
+    }
+  }).then(
+    () => res.redirect('/message'),
+    (err) => res.send(err)
+  )
 }
 //end delete messages
 
@@ -95,7 +109,7 @@ const deleteMessage = (req, res) => {
     User.findAll().then(
         (data) => {
                  if(req.xhr){
-                    res.send(data)    
+                    res.send(data)
                 } else {
                     res.render('show_message', { data })
                 }
@@ -106,12 +120,12 @@ const deleteMessage = (req, res) => {
 } */
 
 module.exports = {
-    showMessage,
-    getId,
-    newMessage,
-    postMessage,
-    getMessage,
-    editMessage,
-    updateMessage,
-    deleteMessage
+  showMessage,
+  getId,
+  newMessage,
+  postMessage,
+  getMessage,
+  editMessage,
+  updateMessage,
+  deleteMessage
 }
